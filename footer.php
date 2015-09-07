a00198d316aab949e74d24e3e1fd17bb449515bf
Tony <chiangloveslynn@gmail.com>
2015-09-07 23:34:22 +0800

    no message

--- footer.php
+++ footer.php
@@ -63,7 +63,17 @@
       js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=136783466462713&version=v2.0";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
-
+    <!-- TypeKit -->
+    <script>
+		  (function(d) {
+		    var config = {
+		      kitId: 'mqt2kok',
+		      scriptTimeout: 3000,
+		      async: true
+		    },
+		    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+		  })(document);
+		</script>
     <?php
       if (!is_single() || !is_page()):
       // our jQuery function here
@@ -93,7 +103,7 @@
                   success: function(html){
                   	$('a#inifiniteLoader').hide('1000');
                       $("#main").append(html);   // This will be the div where our content will be loaded
-                      _jf.flush();
+                      // _jf.flush();
                   }
               });
           return false;
